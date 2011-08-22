91763037426de633a55a56faffa195e9e8829dd1
Austin Brown <agustinmichaelbrown@gmail.com>
2011-08-22 13:25:05 -0400

    Adding config folder

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Agustin::Application.config.secret_token = '9da051723b82cf2022f136871321055556247e59c9c37f778a9f7729b5fa07ab68923dce722e6317644ba7d1e1d0a5a0dfb415f4414e030df046adc9f3b9534f'
